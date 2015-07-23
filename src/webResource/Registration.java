733c503cb366615c28bc305cedece12ff2d5b44e
kingpeterson <kingpetersonwang@gmail.com>
2015-07-22 21:38:21 -0700

    added update function

--- src/webResource/Registration.java
+++ src/webResource/Registration.java
@@ -34,4 +34,21 @@ public class Registration {
 
 	}
 	
+	@POST
+	@Path("/Update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response Update(String input){
+		String result = "";
+		try{
+			JSONObject obj = new JSONObject(input);
+			String objectID = obj.getString("ObjectID");
+			String newInstance = obj.getString("NewInstance");
+			String newValue = obj.getString("NewValue");
+			result = Integer.toString(MongoDb.update(objectID, newInstance, newValue));
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+		return Response.status(201).entity(result).build();
+	}
 }
